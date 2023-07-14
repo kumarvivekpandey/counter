e9c1f4ba764d37c7492b61781636886d0ae1413a
kumarvivekpandey <35356580+kumarvivekpandey@users.noreply.github.com>
2023-07-14 07:44:20 +0000

    changes done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
+    const [value, setValue] = useState(0);
+  const step = useRef(1);
+
+  const setStep = (value: number) => {
+    step.current = value;
+  };
+
+  const increment = () => {
+    setValue((state) => state + step.current);
+  };
+
+  const decrement = () => {
+    setValue((state) => state - step.current);
+  };
+
+  const reset = () => {
+    setValue(0);
+  };
   return (
     <div className="App">
-     
+      <main className={styles.main}>
+      <h2>{value}</h2>
+
+      <div>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+
+      <div>
+        <label htmlFor="step">Icrement/Decrement by</label>
+        <input
+          type="number"
+          id="step"
+          defaultValue={step.current}
+          onChange={(e) =>
+            setStep((e.target as HTMLInputElement).valueAsNumber)
+          }
+        />
+      </div>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </main>
     </div>
   );
 }
